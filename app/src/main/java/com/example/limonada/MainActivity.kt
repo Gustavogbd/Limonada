d42e6a52b4fa6929050f5f16fbb42ef0a20c90f2
Gustavogbd <gustavokkg2@hotmail.com>
2023-06-04 13:50:04 -0300

    Base funcional do app

--- app/src/main/java/com/example/limonada/MainActivity.kt
+++ app/src/main/java/com/example/limonada/MainActivity.kt
@@ -1,46 +1,140 @@
 package com.example.limonada
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.limonada.ui.theme.LimonadaTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LimonadaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                LemonApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonApp() {
+
+    var currentStep by remember { mutableStateOf(1) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = stringResource(id = R.string.quote_1))
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_tree),
+                        contentDescription = stringResource(id = R.string.lemon_tree),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 2
+                            }
+                    )
+                }
+            }
+
+            2 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = stringResource(id = R.string.quote_2))
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_squeeze),
+                        contentDescription = stringResource(id = R.string.lemon),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 3
+                            }
+                    )
+                }
+            }
+
+            3 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = stringResource(id = R.string.quote_3))
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_drink),
+                        contentDescription = stringResource(id = R.string.glass_of_lemonade),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 4
+                            }
+                    )
+                }
+            }
+            4 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = stringResource(id = R.string.quote_4))
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_restart),
+                        contentDescription = stringResource(id = R.string.empty_glass),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 1
+                            }
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LimonadaTheme {
-        Greeting("Android")
+        LemonApp()
     }
 }
